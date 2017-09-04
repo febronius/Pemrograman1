ce80ee81414abd14532d38e8c29da0bb92c186c9
febronius <31607947+febronius@users.noreply.github.com>
2017-09-04 11:47:50 +0700

    Add files via upload

---
+++ Belajar01.java
@@ -0,0 +1,6 @@
+public class Belajar01 {
+    public static void main(String[] args) {
+        System.out.println("\nNama : Febronius Mariano Lbhe");
+        System.out.println("Nim : 17220013 \n");
+    }
+}
---
+++ Belajar02.java
@@ -0,0 +1,11 @@
+public class Belajar02 {
+    public static void main(String[] args) {
+        double berat;
+        berat = 40.2;
+        System.out.println("Berat awal = " + berat);
+        berat = berat + 2;
+        System.out.println("Berat setelah makan = " + berat);
+        berat = berat - 1;
+        System.out.println("Berat pas tanggal tua = " + berat);
+    }
+}
